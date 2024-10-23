b36c5727f998c42a069099077e5dcada2ad87370
Abaranasia <rankirlian@gmail.com>
2024-10-23 23:46:34 +0200

    add FavPokemonCard Solid component

---
+++ src/components/pokemons/FavPokemonCard.tsx
@@ -0,0 +1,39 @@
+import { createSignal, Show, type Component } from "solid-js";
+import type { FavouritePokemon } from "@interfaces/favoritePokemons";
+
+interface Props {
+  favPokemon: FavouritePokemon;
+}
+export const FavPokemonCard: Component<Props> = (props) => {
+  const { favPokemon } = props;
+
+  const [isVisible, setIsVisible] = createSignal(true);
+
+  const imageSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${favPokemon.id}.png`;
+
+  const handleDelete = () => {
+    const favorites: FavouritePokemon[] = JSON.parse(
+      localStorage.getItem("favorites") ?? "[]"
+    );
+
+    const newFavorites = favorites.filter((p) => p.id !== favPokemon.id);
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    setIsVisible(false)
+  };
+
+  return (
+    <Show when={isVisible()}>
+      <div class="flex flex-col justify-center items-center">
+        <a href={`/pokemons/${favPokemon.name}`}>
+          <img src={imageSrc} alt={favPokemon.name} width="100" height="auto" />
+          <p class="capitalize">
+            #{favPokemon.id} - {favPokemon.name}
+          </p>
+        </a>
+        <button class="text-red-400" onclick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </Show>
+  );
+};
